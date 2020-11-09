bbc277c1be94e2a51bfad682b2bb2e1d0b886520
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-11-09 13:44:13 +0100

    icon added

--- src/components/CityItem.js
+++ src/components/CityItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const CityItem = (props) => {
     return (
@@ -7,6 +8,7 @@ const CityItem = (props) => {
             onPress = {props.onSelect}
             style={styles.conta}
         >
+        <Icon name={'university'} size={15} color={'red'} style={{marginLeft : 120 ,marginTop : 7 , marginRight : 10}} />
         <Text  style={styles.text}>{props.cityName}</Text>
     </TouchableOpacity>
     )
@@ -18,7 +20,9 @@ const styles = StyleSheet.create({
     conta : {
         margin : 1,
         padding : 5 ,
-        backgroundColor : '#eceff1'
+        backgroundColor : '#eceff1',
+        flexDirection : "row" ,
+        // justifyContent : "space-between"
     },
     text : {
         // fontWeight : "bold",
